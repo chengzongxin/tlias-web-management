d2ffea0ff8b8f79c7e3aaa87a2624a15fcd3ec99
Joe.cheng <joe.cheng@corp.to8to.com>
2024-03-18 17:33:35 +0800

    cros origins

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -4,11 +4,13 @@ package org.example.config;
 import org.example.interceptor.LoginCheckInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration //配置类
 public class WebConfig implements WebMvcConfigurer {
+    static final String ORIGINS[] = new String[] { "GET", "POST", "PUT", "DELETE" };
 
     @Autowired
     private LoginCheckInterceptor loginCheckInterceptor;
@@ -16,5 +18,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor).addPathPatterns("/**").excludePathPatterns("/login");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        /**
+         * 所有请求都允许跨域，使用这种配置就不需要
+         * 在interceptor中配置header了
+         */
+        corsRegistry.addMapping("/**") // 所有的当前站点的请求地址，都支持跨域访问。
+                .allowCredentials(true)// 是否支持跨域用户凭证
+                .allowedOrigins("http://localhost:9529")// 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")// 当前站点支持的跨域请求类型是什么
+                .allowedHeaders("*")
+                .maxAge(3600);// 超时时长设置为1小时。 时间单位是秒。
     }
 }
