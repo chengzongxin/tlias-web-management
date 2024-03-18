e7a1c5731f51fe683e7ce0ff983e966b6b3ddd00
Joe.cheng <joe.cheng@corp.to8to.com>
2024-03-18 18:07:33 +0800

    cros origins

--- src/main/java/org/example/config/WebConfig.java
+++ src/main/java/org/example/config/WebConfig.java
@@ -27,8 +27,9 @@ public class WebConfig implements WebMvcConfigurer {
          * 在interceptor中配置header了
          */
         corsRegistry.addMapping("/**") // 所有的当前站点的请求地址，都支持跨域访问。
-                .allowCredentials(true)// 是否支持跨域用户凭证
-                .allowedOrigins("http://localhost:9529")// 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
+                // allowOrigins 使用通配符时需要设置为false
+                .allowCredentials(false)// 是否支持跨域用户凭证
+                .allowedOrigins("*")// 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")// 当前站点支持的跨域请求类型是什么
                 .allowedHeaders("*")
                 .maxAge(3600);// 超时时长设置为1小时。 时间单位是秒。
