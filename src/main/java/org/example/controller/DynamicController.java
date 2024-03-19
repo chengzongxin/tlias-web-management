8a48eb11f81f40e1138b23a9a7c887ab773f2b66
Joe.cheng <joe.cheng@corp.to8to.com>
2024-03-19 17:48:06 +0800

    add dynamic requestbody

--- src/main/java/org/example/controller/DynamicController.java
+++ src/main/java/org/example/controller/DynamicController.java
@@ -5,6 +5,7 @@ import org.example.pojo.Result;
 import org.example.service.DynamicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class DynamicController {
     @Autowired
     DynamicService dynamicService;
     @PostMapping
-    public Result add(Dynamic dynamic)
+    public Result insert(@RequestBody Dynamic dynamic)
     {
         dynamicService.insert(dynamic);
         return Result.success();
--- src/main/java/org/example/pojo/Dynamic.java
+++ src/main/java/org/example/pojo/Dynamic.java
@@ -1,4 +1,15 @@
 package org.example.pojo;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Dynamic {
+    private Integer id; //ID
+    private String imgUrl; //
+    private String content; //
 }
